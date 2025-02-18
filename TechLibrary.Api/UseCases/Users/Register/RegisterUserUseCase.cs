d3df025a984e4073f1f700d0d3bdc7c68b9b1607
danieldpalma <danielpalma232@gmail.com>
2025-02-18 18:32:52 -0300

    Aula 01 - Iniciando projeto

---
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -0,0 +1,46 @@
+﻿using TechLibrary.Api.Domain.Entities;
+using TechLibrary.Api.Infrastructure;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.UseCases.Users.Register;
+
+public class RegisterUserUseCase
+{
+    public ResponseRegisterUserJson Execute(RequestUserJson request)
+    {
+        Validate(request);
+
+        var entity = new User
+        {
+            Name = request.Name,
+            Email = request.Email,
+            Password = request.Password
+        };
+
+        var dbContext = new TechLibraryDbContext();
+
+        dbContext.Users.Add(entity);
+
+        dbContext.SaveChanges();
+
+        return new ResponseRegisterUserJson
+        {
+            Name = entity.Name,
+        };
+    }
+
+    private static void Validate(RequestUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+        var result = validator.Validate(request);
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
